438a737792940826bbc1ee9e4edfa177e592203e
Angel Reyes <angelmrg2000@hotmail.com>
2020-07-29 21:50:12 -0600

    components/card/card.component: added variables to the cards (id, username)

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,6 +5,8 @@ export const Card = props => (
     <div className='card-container'>
     <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
      <h2>{ props.monster.name }</h2>
+     <p> {props.monster.username} </p>
      <p> { props.monster.email } </p>
+     <p>{props.monster.id}</p>
     </div>
 );
