56e9b572e43d7e427515d3e8b9c2c06a646f0b2b
Angel Reyes <angelmrg2000@hotmail.com>
2020-08-03 13:33:04 -0600

    Updates

---
+++ precache-manifest.ea5957cb7f64d714249f94cbabf9f001.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73bf45cd40c38d04ad6c50dddb769efa",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "d56dc4e9bef251c6c371",
+    "url": "/Monsters-Rolodex/static/css/main.5383a25a.chunk.css"
+  },
+  {
+    "revision": "0c93387514e954bdf57e",
+    "url": "/Monsters-Rolodex/static/js/2.605ea4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.605ea4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d56dc4e9bef251c6c371",
+    "url": "/Monsters-Rolodex/static/js/main.12cdc9e4.chunk.js"
+  },
+  {
+    "revision": "f79345753b7bd810d938",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.a9daa82d.js"
+  }
+]);
